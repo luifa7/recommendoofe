284cfc9659abe84d01a1e07674a4fd3cb3c7886b
lgodoyalvarez <luis.godoy.alvarez@paessler.com>
2021-12-27 16:49:58 +0100

    commit footer error

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,12 +2,5 @@ import RecommendationCard from "./RecommendationCard.vue";
 import CityCard from "./CityCard.vue";
 import GoogleMapsIframe from "./GoogleMapsIframe.vue";
 import DetailsInfoCard from "./DetailsInfoCard.vue";
-import Footer from "./Footer.vue";
 
-export {
-  RecommendationCard,
-  CityCard,
-  GoogleMapsIframe,
-  DetailsInfoCard,
-  Footer,
-};
+export { RecommendationCard, CityCard, GoogleMapsIframe, DetailsInfoCard };
