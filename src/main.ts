607e964fac1481860d5c7a47530ca889a5a80252
lgodoyalvarez <luis.godoy.alvarez@paessler.com>
2021-12-27 17:23:59 +0100

    define external footer

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import Footer from "./Footer.vue";
 import router from "./router";
 import { store } from "./store";
 import "./assets/css/styles.css";
 
 createApp(App).use(store).use(router).mount("#app");
+createApp(Footer).mount("#footer");
