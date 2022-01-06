e715dd67ed6fe4fadcb73cd0b077a9b2e8dc5f99
luifa7 <luifa.luifa@gmail.com>
2022-01-06 19:05:06 +0100

    remove unused code

--- src/views/RegisterUser.vue
+++ src/views/RegisterUser.vue
@@ -145,24 +145,6 @@
                 <label for="password">Repeat password</label>
                 <div class="invalid-feedback">Your password is required.</div>
               </div>
-              <!-- Submit success message-->
-              <!---->
-              <!-- This is what your users will see when the form-->
-              <!-- has successfully submitted-->
-              <div class="d-none" id="submitSuccessMessage">
-                <div class="text-center mb-3">
-                  <div class="fw-bolder">Form submission successful!</div>
-                </div>
-              </div>
-              <!-- Submit error message-->
-              <!---->
-              <!-- This is what your users will see when there is-->
-              <!-- an error submitting the form-->
-              <div class="d-none" id="submitErrorMessage">
-                <div class="text-center text-danger mb-3">
-                  Error sending message!
-                </div>
-              </div>
               <!-- Submit Button-->
               <div class="d-grid">
                 <button
