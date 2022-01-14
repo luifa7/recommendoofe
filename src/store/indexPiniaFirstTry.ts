33b994547b5e635903c4a60f91e7cae3dd7ecfc9
luifa7 <luifa.luifa@gmail.com>
2022-01-14 23:32:34 +0100

    refactor: first aproach of pinia

---
+++ src/store/indexPiniaFirstTry.ts
@@ -0,0 +1,34 @@
+import { defineStore } from "pinia";
+import { Ref, ref } from "vue";
+import { City, User } from "./types/types";
+
+export const useUserStore = defineStore("user", () => {
+  const loggedInUser = ref();
+  const userFriends: Ref<Array<User>> = ref([]);
+  const userCities: Ref<Array<City>> = ref([]);
+
+  function loginUser(user: User) {
+    loggedInUser.value = user;
+  }
+  function logoutUser() {
+    userFriends.value = [];
+    userCities.value = [];
+    loggedInUser.value = undefined;
+  }
+  function setLoggedUserFriends(friends: Array<User>) {
+    userFriends.value = friends;
+  }
+  function setLoggedUserCities(cities: Array<City>) {
+    userCities.value = cities;
+  }
+
+  return {
+    loggedInUser,
+    userFriends,
+    userCities,
+    loginUser,
+    logoutUser,
+    setLoggedUserFriends,
+    setLoggedUserCities,
+  };
+});
