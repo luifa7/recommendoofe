f525e075baab1a53161b304887d17cd14cf36c73
luifa7 <luifa.luifa@gmail.com>
2022-01-19 22:08:37 +0100

    feat: add side bar to settings page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,29 +1,104 @@
 <template>
-  <!-- Page Content-->
-  <section class="py-3">
-    <div class="my-4">
-      <div class="text-center mb-5">
-        <h1 class="logo-font">
-          <span>Settings</span>
-        </h1>
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+    <!-- Sidebar -->
+    <ul
+      class="navbar-nav bg-gradient-body-color sidebar sidebar-dark accordion"
+      id="accordionSidebar"
+    >
+      <!-- Sidebar - Brand -->
+      <a
+        class="sidebar-brand d-flex align-items-center justify-content-center"
+        href="#"
+      >
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="bi bi-sliders"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0" />
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item active">
+        <a class="nav-link" href="#">
+          <i class="bi bi-sliders"></i>
+          <span>Dashboard</span></a
+        >
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider" />
+
+      <!-- Heading -->
+      <div class="sidebar-heading">Interface</div>
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="bi bi-sliders"></i>
+          <span>Components</span>
+        </a>
+      </li>
+
+      <!-- Nav Item - Utilities Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="bi bi-sliders"></i>
+          <span>Utilities</span>
+        </a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider" />
+
+      <!-- Heading -->
+      <div class="sidebar-heading">Addons</div>
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="bi bi-sliders"></i>
+          <span>Pages</span>
+        </a>
+      </li>
+
+      <!-- Nav Item - Charts -->
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="bi bi-sliders"></i>
+          <span>Charts</span></a
+        >
+      </li>
+
+      <!-- Nav Item - Tables -->
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="bi bi-sliders"></i>
+          <span>Tables</span></a
+        >
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block" />
+    </ul>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <!-- Main Content -->
+      <div id="content">
+        <!-- Begin Page Content -->
+        <div class="container-fluid"></div>
+        <!-- /.container-fluid -->
       </div>
-      <div>
-        <table class="table table-dark table-hover">
-          <tbody>
-            <tr>
-              <td>Mark</td>
-            </tr>
-            <tr>
-              <td>Jacob</td>
-            </tr>
-            <tr>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <!-- End of Main Content -->
     </div>
-  </section>
+    <!-- End of Content Wrapper -->
+  </div>
+  <!-- End of Page Wrapper -->
 </template>
 
 <script lang="ts" setup></script>
+<style scoped src="@/assets/css/styles-admin.css"></style>
